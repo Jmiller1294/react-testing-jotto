8598106b6af2ecdb31e0c92482e8499a65052a24
Justin Miller <jayman1294@gmail.com>
2021-08-01 19:50:07 -0400

    added cpropTypes library

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'; 
+import { checkPropTypes } from 'prop-types';
 import { findByTestAttr } from '../test/testUtils';
+
 
 import Congrats from './Congrats';
 
@@ -27,4 +29,10 @@ test('renders non-empty congrats message when success prop is true', () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, 'congrats-message');
   expect(message.text().length).not.toBe(0);
+})
+
+test('does not throw warning with expected props', () => {
+  const expectedProps = { success: false };
+  const propError = checkPropTypes(Congrats.propTypes, expectedProps, 'prop', Congrats.name);
+  expect(propError).toBeUndefined();
 })
