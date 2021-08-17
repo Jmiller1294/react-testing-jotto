5085371af711d3f0ca7321b05aa6b678188a96cc
Justin Miller <jayman1294@gmail.com>
2021-08-16 23:44:31 -0400

    created tests for testing state controlled inputs

--- src/Input.test.js
+++ src/Input.test.js
@@ -5,18 +5,35 @@ import { findByTestAttr, checkProps } from "../test/testUtils";
 import Input from "./Input";
 
 const defaultProps = { secretWord: 'hello'}
+const mockSetCurrentGuess = jest.fn();
+
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useState: (initialState) => [initialState, mockSetCurrentGuess]
+}))
 
 const setup = (props={}) => {
   const setupProps = {...defaultProps, ...props};
   return shallow(<Input { ...setupProps } />);
 }
 
+test('Input component renders without error', () => {
+  const wrapper = setup();
+  const inputComponent = findByTestAttr(wrapper, 'component-input');
+  expect(inputComponent.length).toBe(1);
+})
+
 test('does not throw warning with expected props', () => {
   checkProps(Input, defaultProps);
 })
 
-test('renders without error', () => {
-  const wrapper = setup();
-  const inputComponent = findByTestAttr(wrapper, 'component-input');
-  expect(inputComponent.length).toBe(1);
+describe('state controlled input field', () => {
+  test('state updates with value of input box upon change', () => {
+    const wrapper = setup();
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+    const mockEvent = { target: { value: 'train'} };
+  
+    inputBox.simulate('change', mockEvent);
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  })
 })
