8323fb0938577264d09374ec202a1a8e6352bf17
Justin Miller <jayman1294@gmail.com>
2021-08-23 23:47:13 -0400

    created setup for moxios

---
+++ src/actions/index.test.js
@@ -0,0 +1,14 @@
+import moxios from 'moxios';
+
+describe('getScretWord', () => {
+  beforeEach(() => {
+    moxios.install();
+  })
+  afterEach(() => {
+    moxios.uninstall();
+  })
+
+  test('secretWord is returned from the function', () => {
+    
+  })
+})
