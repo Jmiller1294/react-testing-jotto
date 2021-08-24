7f6bdfc856d76179cae1895db72e08cf3ba96ff3
Justin Miller <jayman1294@gmail.com>
2021-08-23 23:10:35 -0400

    added functional tests for guessword

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
+import Input from './Input';
 
 const words = [
   { guessedWord: 'train', letterMatchCount: 3 }
 ]
 
 function App() {
+  const success = false;
+  const secretWord = 'party';
+  const guessedWords = [];
+
   return (
     <div data-test="component-app" className="container">
     <h2>Jotto</h2>
-      <Congrats success={false} />
-      <GuessedWords guessedWords={words}/>
+      <Congrats success={success} />
+      <Input success={success} secretWord={secretWord} />
+      <GuessedWords guessedWords={guessedWords}/>
     </div>
   );
 }
--- src/guessWord.test.js
+++ src/guessWord.test.js
@@ -4,7 +4,7 @@ import { findByTestAttr } from '../test/testUtils';
 import App from './App';
 
 const setup = (state = {}) => {
-  const wrapper= mount(<App />);
+  const wrapper = mount(<App />);
 
   //add value to input box
   const inputBox = findByTestAttr(wrapper, 'input-box');
@@ -17,14 +17,49 @@ const setup = (state = {}) => {
   return wrapper;
 }
 
-describe('no words guessed', () => {
-
+describe.skip('no words guessed', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: 'party',
+      success: false,
+      guessedWords: []
+  })
+})
+  test('creates GuessedWords table with one row', () => {
+    const GuessedWordRows = findByTestAttr(wrapper, 'guessed-word');
+    expect(GuessedWordRows).toHaveLength(1);
+  })
 })
 
-describe('some words guessed', () => {
-
+describe.skip('some words guessed', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: 'party',
+      success: false,
+      guessedWords: [{ guessedWord: 'agile', letterMatchCount: 1 }]
+    })
+  })
+  test('creates a GuessedWords table with multiple rows', () => {
+    const GuessedWordRows = findByTestAttr(wrapper, 'guessed-word');
+    expect(GuessedWordRows).toHaveLength(2)
+  })
 })
 
-describe('guess secret word', () => {
-  
+describe.skip('guess secret word', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: 'party',
+      success: false,
+      guessedWords: [ { guessedWord: 'agile', letterMatchCount: 1 } ]
+    })
+  })
+  test('creates a GuessedWords table with multiple rows', () => {
+    const GuessedWordRows = findByTestAttr(wrapper, 'guessed-word');
+    const CongratsMessage = findByTestAttr(wrapper, 'congrats-message')
+    expect(GuessedWordRows).toHaveLength(3)
+    expect(CongratsMessage.text().length).not.toBe(0);
+  })
 })
